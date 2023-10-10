b6a87250cd5685e319582cc7934916ce096ac9c5
A B Veekshith <87522608+ABveekshith@users.noreply.github.com>
2023-10-10 15:57:26 +0530

    Create Tic_Tac_Toe.py

---
+++ Tic Tac Toe/Tic_Tac_Toe.py
@@ -0,0 +1,52 @@
+# Tic-Tac-Toe 
+
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_winner(board, player):
+    for row in board:
+        if all([cell == player for cell in row]):
+            return True
+
+    for col in range(3):
+        if all([board[row][col] == player for row in range(3)]):
+            return True
+
+    if all([board[i][i] == player for i in range(3)]) or all([board[i][2 - i] == player for i in range(3)]):
+        return True
+
+    return False
+
+def is_full(board):
+    return all([cell != ' ' for row in board for cell in row])
+
+def play_game():
+    board = [[' ' for _ in range(3)] for _ in range(3)]
+    current_player = 'X'
+
+    while True:
+        print_board(board)
+        row = int(input(f"Player {current_player}, enter row (0, 1, or 2): "))
+        col = int(input(f"Player {current_player}, enter column (0, 1, or 2): "))
+
+        if row < 0 or row > 2 or col < 0 or col > 2 or board[row][col] != ' ':
+            print("Invalid move! Try again.")
+            continue
+
+        board[row][col] = current_player
+
+        if check_winner(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        elif is_full(board):
+            print_board(board)
+            print("It's a tie!")
+            break
+
+        current_player = 'O' if current_player == 'X' else 'X'
+
+if __name__ == "__main__":
+    play_game()
